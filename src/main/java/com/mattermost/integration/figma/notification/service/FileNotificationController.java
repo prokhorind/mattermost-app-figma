bb6fb532086d2482c6f296fdce2cacf399360f34
prokhorind <prokhorind@gmail.com>
2022-04-21 15:37:45 +0300

    pr review changes

--- src/main/java/com/mattermost/integration/figma/notification/service/FileNotificationController.java
+++ src/main/java/com/mattermost/integration/figma/notification/service/FileNotificationController.java
@@ -4,7 +4,6 @@ package com.mattermost.integration.figma.notification.service;
 import com.mattermost.integration.figma.api.mm.kv.KVService;
 import com.mattermost.integration.figma.input.oauth.InputPayload;
 import lombok.extern.slf4j.Slf4j;
-import net.steppschuh.markdowngenerator.table.Table;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
