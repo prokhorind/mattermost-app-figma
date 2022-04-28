af9c836bbbe14d55cab062f3931bde1ec18675ba
Ostap Melnychuk <ostapss222@gmail.com>
2022-04-28 15:31:50 +0300

    Encrypting added

--- src/main/java/com/mattermost/integration/figma/api/mm/kv/UserDataKVServiceImpl.java
+++ src/main/java/com/mattermost/integration/figma/api/mm/kv/UserDataKVServiceImpl.java
@@ -3,27 +3,44 @@ package com.mattermost.integration.figma.api.mm.kv;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.mattermost.integration.figma.input.oauth.InputPayload;
 import com.mattermost.integration.figma.security.dto.UserDataDto;
+import com.mattermost.integration.figma.security.service.DataEncryptionService;
 import com.mattermost.integration.figma.utils.json.JsonUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class UserDataKVServiceImpl implements UserDataKVService {
     private final KVService kvService;
     private final JsonUtils jsonUtils;
+    private final DataEncryptionService dataEncryptionService;
 
-    public UserDataKVServiceImpl(KVService kvService, JsonUtils jsonUtils) {
+    public UserDataKVServiceImpl(KVService kvService, JsonUtils jsonUtils, DataEncryptionService dataEncryptionService) {
         this.kvService = kvService;
         this.jsonUtils = jsonUtils;
+        this.dataEncryptionService = dataEncryptionService;
     }
 
     public UserDataDto getUserData(String userId, String mmSiteUrl, String botAccessToken) {
-        return (UserDataDto) jsonUtils.convertStringToObject(kvService.get(userId, mmSiteUrl,
+        UserDataDto currentUserData = (UserDataDto) jsonUtils.convertStringToObject(kvService.get(userId, mmSiteUrl,
                 botAccessToken), UserDataDto.class).get();
+        try {
+            currentUserData.setClientSecret(dataEncryptionService.decrypt(currentUserData.getClientSecret()));
+            currentUserData.setRefreshToken(dataEncryptionService.decrypt(currentUserData.getRefreshToken()));
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            log.error(e.getMessage());
+        }
+        return currentUserData;
     }
 
     public Set<String> getUserIdsByTeamId(String teamId, String mmSiteUrl, String botAccessToken) {
@@ -64,9 +81,15 @@ public class UserDataKVServiceImpl implements UserDataKVService {
         String botAccessToken = inputPayload.getContext().getBotAccessToken();
 
         currentData.setClientId(inputPayload.getContext().getOauth2().getClientId());
-        currentData.setClientSecret(inputPayload.getContext().getOauth2().getClientSecret());
-        currentData.setRefreshToken(inputPayload.getContext().getOauth2().getUser().getRefreshToken());
+        try {
+            currentData.setClientSecret(dataEncryptionService.encrypt(inputPayload.getContext().getOauth2().getClientSecret()));
+            currentData.setRefreshToken(dataEncryptionService.encrypt(inputPayload.getContext().getOauth2().getUser().getRefreshToken()));
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            log.error(e.getMessage());
+        }
+
         currentData.setMmUserId(inputPayload.getContext().getActingUser().getId());
+
         kvService.put(userId, currentData, mmSiteUrl, botAccessToken);
     }
 }
