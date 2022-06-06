7b4faf925c6327c4aebfea025f282fa29facd30c
prokhorind <prokhorind@gmail.com>
2022-06-06 14:28:37 +0300

    Change figma bad request exception handling

--- src/main/java/com/mattermost/integration/figma/config/exception/handler/ExceptionHandlingController.java
+++ src/main/java/com/mattermost/integration/figma/config/exception/handler/ExceptionHandlingController.java
@@ -1,5 +1,6 @@
 package com.mattermost.integration.figma.config.exception.handler;
 
+import com.mattermost.integration.figma.config.exception.exceptions.figma.FigmaBadRequestException;
 import com.mattermost.integration.figma.config.exception.exceptions.figma.FigmaBasicTeamSubscriptionException;
 import com.mattermost.integration.figma.config.exception.exceptions.figma.FigmaResourceForbiddenException;
 import com.mattermost.integration.figma.config.exception.exceptions.figma.FigmaResourceNotFoundException;
@@ -9,6 +10,7 @@ import com.mattermost.integration.figma.config.exception.exceptions.mm.MMSubscri
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -20,6 +22,20 @@ public class ExceptionHandlingController extends ResponseEntityExceptionHandler 
     public String handleFigmaResourceNotFoundException(
             RuntimeException ex, WebRequest request) {
         return "{\"type\":\"error\",\"text\":\"Figma resource was not found\"}";
+    }
+
+    @ExceptionHandler(value = {FigmaBadRequestException.class})
+    @ResponseBody
+    public String handleFigmaBadRequestException(RuntimeException ex, WebRequest request) {
+
+        String requestUrl = ((ServletWebRequest) request).getRequest().getRequestURI();
+        String[] path = requestUrl.split("/");
+        String url = path[path.length - 1];
+        String message = ex.getMessage();
+        if ("projects".equals(url) || "file".equals(url)) {
+            message = new FigmaBasicTeamSubscriptionException().getMessage();
+        }
+        return String.format("{\"type\":\"error\",\"text\":\"%s\"}", message);
     }
 
     @ExceptionHandler(value = {FigmaResourceForbiddenException.class})
